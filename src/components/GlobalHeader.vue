d7059a116918de275c70a571f5066a1b9441a8d4
www <2316754787@qq.com>
2025-02-17 22:55:27 +0800

    通用布局和全局状态管理

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,71 @@
+<template>
+  <a-row id="globalHeader" style="margin-bottom: 16px" align="center">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img
+              class="logo"
+              src="http://47.107.242.254/images/avatar.png"
+              alt=""
+            />
+            <div class="title">W OJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in routes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>{{ currentUser.userName }}</div>
+    </a-col>
+  </a-row>
+</template>
+<script setup lang="ts">
+import { routes } from "@/router/router";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const router = useRouter();
+const selectedKeys = ref(["/"]);
+
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+const doMenuClick = (key: string) => {
+  router.push({ path: key });
+};
+
+const store = useStore();
+const currentUser = store.state.user?.loginUser;
+</script>
+
+<style scoped>
+#globalHeader {
+}
+
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
---
+++ src/store/user.ts
@@ -0,0 +1,21 @@
+// initial state
+import { StoreOptions } from "vuex";
+
+export default {
+  namespaced: true,
+  state: () => ({
+    loginUser: {
+      userName: "未登录",
+    },
+  }),
+  actions: {
+    getLoginUser({ commit, state }, payload) {
+      commit("updateUser", { userName: "W" });
+    },
+  },
+  mutations: {
+    updateUser(state, payload) {
+      state.loginUser = payload;
+    },
+  },
+} as StoreOptions<any>;
